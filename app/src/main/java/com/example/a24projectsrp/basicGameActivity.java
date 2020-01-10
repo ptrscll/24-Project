ed4c411f187e20052433d91fef7689024aa76801
ptrscll <ptrscll@gmail.com>
2020-01-09 21:36:55 -0500

    Finished basic game

--- app/src/main/java/com/example/a24projectsrp/basicGameActivity.java
+++ app/src/main/java/com/example/a24projectsrp/basicGameActivity.java
@@ -24,6 +24,10 @@ public class basicGameActivity extends AppCompatActivity {
         generateNewNums();
     }
 
+    //Setting global vars
+    public static boolean opsEntered = false;
+    public static boolean numsEntered = false;
+
     public void generateNewNums(){
         Random rand = new Random();
         Button num1 = (Button)findViewById(R.id.btnNum1);
@@ -55,18 +59,145 @@ public class basicGameActivity extends AppCompatActivity {
         }
     }
 
+    //Function used in addNum/addOp to check if all inputs are entered and disable/enable buttons
+    public void checkInputs(){
+        if(numsEntered == true && opsEntered == true){
+            Button btnChecker = findViewById(R.id.btnCheck);
+            Button btnPlus = findViewById(R.id.btnAdd);
+            Button btnMinus = findViewById(R.id.btnSub);
+            Button btnTimes = findViewById(R.id.btnMult);
+            Button btnDivBy = findViewById(R.id.btnDiv);
+            btnChecker.setEnabled(true);
+            btnPlus.setEnabled(false);
+            btnMinus.setEnabled(false);
+            btnTimes.setEnabled(false);
+            btnDivBy.setEnabled(false);
+
+        }
+    }
+
+    //Function to call in number Buttons
+    public void addNum(Button btnNewNum){
+        TextView txtNum1 = findViewById(R.id.txtNum1);
+        TextView txtNum2 = findViewById(R.id.txtNum2);
+        TextView txtNum3 = findViewById(R.id.txtNum3);
+        TextView txtNum4 = findViewById(R.id.txtNum4);
+        TextView[] txtNums = {txtNum1, txtNum2, txtNum3, txtNum4};
+        boolean notBlank = true;
+        int i = 0;
+        while (notBlank && i < 4) {
+            if(txtNums[i].getText().toString().equals("_")){
+                notBlank = false;
+                txtNums[i].setText(btnNewNum.getText());
+                btnNewNum.setEnabled(false);
+                if(i == 3){
+                    numsEntered = true;
+                }
+            }
+            i++;
+        }
+        checkInputs();
+    }
+
+    //Function to call in operation buttons
+    public void addOp(Button btnNewOp){
+        TextView txtOp1 = findViewById(R.id.txtOp1);
+        TextView txtOp2 = findViewById(R.id.txtOp2);
+        TextView txtOp3 = findViewById(R.id.txtOp3);
+        TextView[] txtOps = {txtOp1, txtOp2, txtOp3};
+        boolean notBlank = true;
+        int i = 0;
+        while (notBlank && i < 3) {
+            if(txtOps[i].getText().toString().equals("_")){
+                notBlank = false;
+                txtOps[i].setText(btnNewOp.getText());
+                if(i == 2){
+                    opsEntered = true;
+                }
+            }
+            i++;
+        }
+        checkInputs();
+    }
+
+    //Methods for #/operation buttons
+    public void addNum1(View view){
+        addNum((Button) findViewById(R.id.btnNum1));
+    }
+
+    public void addNum2(View view){
+        addNum((Button) findViewById(R.id.btnNum2));
+    }
+
+    public void addNum3(View view){
+        addNum((Button) findViewById(R.id.btnNum3));
+    }
+
+    public void addNum4(View view){
+        addNum((Button) findViewById(R.id.btnNum4));
+    }
+
+    public void addPlus(View view){
+        addOp((Button) findViewById(R.id.btnAdd));
+    }
+
+    public void addMinus(View view){
+        addOp((Button) findViewById(R.id.btnSub));
+    }
+
+    public void addTimes(View view){
+        addOp((Button) findViewById(R.id.btnMult));
+    }
+
+    public void addDivBy(View view){
+        addOp((Button) findViewById(R.id.btnDiv));
+    }
+
+    //Method for clearing textViews
+    public void clearTxts(View view){
+        TextView txtNum1 = findViewById(R.id.txtNum1);
+        TextView txtOp1 = findViewById(R.id.txtOp1);
+        TextView txtNum2 = findViewById(R.id.txtNum2);
+        TextView txtOp2 = findViewById(R.id.txtOp2);
+        TextView txtNum3 = findViewById(R.id.txtNum3);
+        TextView txtOp3 = findViewById(R.id.txtOp3);
+        TextView txtNum4 = findViewById(R.id.txtNum4);
+        TextView[] txts = {txtNum1, txtOp1, txtNum2, txtOp2, txtNum3, txtOp3, txtNum4};
+        for(int i = 0; i < 7; i++){
+            txts[i].setText("_");
+        }
+        Button btnAdd = findViewById(R.id.btnAdd);
+        Button btnSub = findViewById(R.id.btnSub);
+        Button btnMult = findViewById(R.id.btnMult);
+        Button btnDiv = findViewById(R.id.btnDiv);
+        Button btnNum1 = findViewById(R.id.btnNum1);
+        Button btnNum2 = findViewById(R.id.btnNum2);
+        Button btnNum3 = findViewById(R.id.btnNum3);
+        Button btnNum4 = findViewById(R.id.btnNum4);
+        Button[] btns = {btnAdd, btnSub, btnMult, btnDiv, btnNum1, btnNum2, btnNum3, btnNum4};
+        for(int i = 0; i < 8; i++){
+            btns[i].setEnabled(true);
+        }
+        Button btnCheck = findViewById(R.id.btnCheck);
+        btnCheck.setEnabled(false);
+        TextView txtAnswer = findViewById(R.id.txtAnswer);
+        txtAnswer.setText("");
+        opsEntered = false;
+        numsEntered = false;
+    }
+
     //Check if numbers reach 24
     public void checkNums(View view){
         //Creating vars for the textboxes
-        EditText txtNum1 = (EditText)findViewById(R.id.txtNum1);
-        EditText txtOp1 = (EditText)findViewById(R.id.txtOp1);
-        EditText txtNum2 = (EditText)findViewById(R.id.txtNum2);
-        EditText txtOp2 = (EditText)findViewById(R.id.txtOp2);
-        EditText txtNum3 = (EditText)findViewById(R.id.txtNum3);
-        EditText txtOp3 = (EditText)findViewById(R.id.txtOp3);
-        EditText txtNum4 = (EditText)findViewById(R.id.txtNum4);
-        EditText[] txtNums = {txtNum1, txtNum2, txtNum3, txtNum4};
-        EditText[] txtOps = {txtOp1, txtOp2, txtOp3};
+        TextView txtNum1 = findViewById(R.id.txtNum1);
+        TextView txtOp1 = findViewById(R.id.txtOp1);
+        TextView txtNum2 = findViewById(R.id.txtNum2);
+        TextView txtOp2 = findViewById(R.id.txtOp2);
+        TextView txtNum3 = findViewById(R.id.txtNum3);
+        TextView txtOp3 = findViewById(R.id.txtOp3);
+        TextView txtNum4 = findViewById(R.id.txtNum4);
+        TextView[] txtNums = {txtNum1, txtNum2, txtNum3, txtNum4};
+        TextView[] txtOps = {txtOp1, txtOp2, txtOp3};
 
         //Converting textbox info into ints
         List<Integer> arrInts = new ArrayList<>();
